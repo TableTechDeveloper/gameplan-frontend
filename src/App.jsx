75954bb4633a8d80ebf5d8285a18d8e045c3dc56
Elijah <122788521+elijahjf@users.noreply.github.com>
2024-08-05 18:36:43 +1000

    feat: add render route

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,35 @@ import MyDrafts from "./pages/MyDrafts.jsx";
 import MyEvents from "./pages/MyEvents.jsx";
 import NewEvent from "./pages/NewEvent.jsx";
 
+axios.defaults.baseURL = "http://localhost:3000";
+
+// Request interceptor
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("token");
+  console.log("Interceptor - Token from localStorage: ", token);
+
+  // If token exists, add it to the Authorization header
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
+});
 
 const App = () => {
-    
-    return (
-        <Routes>
-            <Route path="/" element={<Template />}>
-                <Route index element={<HomePage />} />
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="discoverevents" element={<DiscoverEvents />} />
-                <Route path="discovergames" element={<DiscoverGames />} />
-                <Route path="editprofile" element={<EditProfile />} />
-                <Route path="eventpage" element={<EventPage />} />
-                <Route path="gamesowned" element={<GamesOwned />} />
-                <Route path="mydrafts" element={<MyDrafts />} />
-                <Route path="myevents" element={<MyEvents />} />
-                <Route path="newevent" element={<NewEvent />} />
-                
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Template />}>
+        <Route index element={<HomePage />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="discoverevents" element={<DiscoverEvents />} />
+        <Route path="discovergames" element={<DiscoverGames />} />
+        <Route path="editprofile" element={<EditProfile />} />
+        <Route path="eventpage" element={<EventPage />} />
+        <Route path="gamesowned" element={<GamesOwned />} />
+        <Route path="mydrafts" element={<MyDrafts />} />
+        <Route path="myevents" element={<MyEvents />} />
+        <Route path="newevent" element={<NewEvent />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
