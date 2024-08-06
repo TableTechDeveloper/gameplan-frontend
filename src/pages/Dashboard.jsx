a8975c91dadaa539df8cebe2f0b286826f2e0b9a
Elijah <122788521+elijahjf@users.noreply.github.com>
2024-08-06 18:55:01 +1000

    feat: add in logged in username to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,46 +1,45 @@
-import React from 'react';
+import React from "react";
 import useFetchEvents from "../functions/useFetchEvents";
 import UpcomingEventCard from "../components/UpcomingEventCard";
-import UserIcon from '../components/UserIcon';
-import { NavLink } from 'react-router-dom';
-
+import UserIcon from "../components/UserIcon";
+import { NavLink } from "react-router-dom";
 
 const Dashboard = () => {
+  const {
+    events,
+    loading: eventsLoading,
+    error: eventsError,
+  } = useFetchEvents(false); // Set hosted to false
 
-    const { events, loading: eventsLoading, error: eventsError } = useFetchEvents(false); // Set hosted to false
+  if (eventsLoading) {
+    return <div>Loading...</div>;
+  }
 
-    if (eventsLoading) {
-        return <div>Loading...</div>;
-    }
+  if (eventsError) {
+    return <div>{eventsError}</div>;
+  }
 
-    if (eventsError) {
-        return <div>{eventsError}</div>;
-    }
-
-    return (
-        <section className="page-section">
-            <div className="page-top">
-                <div>
-                    <h1>Dashboard:</h1>
-                    <UserIcon />
-                </div>
-                <div className="page-buttons">
-                    <NavLink to="/newevent">
-                        <button className="button-primary">New Event</button>
-                    </NavLink>
-                </div>
-            </div>
-            <div className="page-content">
-                <h2>Your upcoming events:</h2>
-                {events.map(event => (
-                    <UpcomingEventCard 
-                        key={event._id}
-                        event={event}
-                    />
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="page-section">
+      <div className="page-top">
+        <div>
+          <h1>Dashboard:</h1>
+          <UserIcon />
+        </div>
+        <div className="page-buttons">
+          <NavLink to="/newevent">
+            <button className="button-primary">New Event</button>
+          </NavLink>
+        </div>
+      </div>
+      <div className="page-content">
+        <h2>Your upcoming events:</h2>
+        {events.map((event) => (
+          <UpcomingEventCard key={event._id} event={event} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
