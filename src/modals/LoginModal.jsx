bc61357cafc14a11d1dde5d6b62e884effb56736
Elijah <122788521+elijahjf@users.noreply.github.com>
2024-08-10 17:05:14 +1000

    fix: redirect for login and register modals

--- src/modals/LoginModal.jsx
+++ src/modals/LoginModal.jsx
@@ -1,12 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "../axios";
 import { ModalContext } from "../pages/_TemplatePage";
 import RegisterModal from "../modals/RegisterModal";
 import ResetPasswordModal from "./ResetPasswordModal";
-import LoginForm from "../components/LoginForm";
 
 const LoginModal = () => {
   const { closeModal, openModal } = useContext(ModalContext);
+  const navigate = useNavigate();
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`/user/login`, {
+        username,
+        password,
+      });
+      console.log("Login successful:", response.data);
+
+      const data = await response.data;
+      const token = data.jwt;
+
+      localStorage.setItem("token", token);
+      console.log(
+        "Token stored in localStorage:",
+        localStorage.getItem("token")
+      );
+
+      // Clear form and error on success
+      setUsername("");
+      setPassword("");
+      setError(null);
+
+      closeModal();
+      navigate("/dashboard");
+    } catch (error) {
+      console.error("Error logging in:", error);
+      // Handle login error
+      if (error.response && error.response.data && error) {
+        setError(error.response.data.message);
+      } else {
+        setError("An error occurred while logging in. Please try again.");
+      }
+    }
+  };
   const handleRegisterClick = (event) => {
     event.preventDefault();
     closeModal();
@@ -28,7 +70,28 @@ const LoginModal = () => {
         </span>
       </p>
 
-      <LoginForm />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <div className="error-message">{error}</div>}
+        <button type="submit">Login</button>
+      </form>
 
       <p className="small-link" onClick={handleForgotPasswordClick}>
         Forgot Password?
