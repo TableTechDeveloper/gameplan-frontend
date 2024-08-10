29d1aabdc3ebab24a16e062c3d7f5b27e10e2bbb
Elijah <122788521+elijahjf@users.noreply.github.com>
2024-08-10 17:05:14 +1000

    fix: redirect for login and register modals

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,73 +1,74 @@
-import React, { useState } from "react";
-import axios from "../axios"; // axios instance is imported from axios.js
-import { useNavigate } from "react-router-dom";
+// DEPRICATED: This file is no longer in use. It has been joined with LoginModal.jsx
+// import React, { useState } from "react";
+// import axios from "../axios"; // axios instance is imported from axios.js
+// import { useNavigate } from "react-router-dom";
 
-const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
+// const LoginForm = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState(null);
+//   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(`/user/login`, {
-        username,
-        password,
-      });
-      console.log("Login successful:", response.data);
-      // Handle successful login (e.g., store token, redirect user)
-      const data = await response.data;
-      const token = data.jwt;
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const response = await axios.post(`/user/login`, {
+//         username,
+//         password,
+//       });
+//       console.log("Login successful:", response.data);
+//       // Handle successful login (e.g., store token, redirect user)
+//       const data = await response.data;
+//       const token = data.jwt;
 
-      // Store the token in localStorage
-      localStorage.setItem("token", token);
-      console.log(
-        "Token stored in localStorage:",
-        localStorage.getItem("token")
-      );
+//       // Store the token in localStorage
+//       localStorage.setItem("token", token);
+//       console.log(
+//         "Token stored in localStorage:",
+//         localStorage.getItem("token")
+//       );
 
-      // Clear form and error on success
-      setUsername("");
-      setPassword("");
-      setError(null);
+//       // Clear form and error on success
+//       setUsername("");
+//       setPassword("");
+//       setError(null);
 
-      navigate("/dashboard");
-    } catch (error) {
-      console.error("Error logging in:", error);
-      // Handle login error
-      if (error.response && error.response.data && error) {
-        setError(error.response.data.message);
-      } else {
-        setError("An error occurred while logging in. Please try again.");
-      }
-    }
-  };
+//       navigate("/dashboard");
+//     } catch (error) {
+//       console.error("Error logging in:", error);
+//       // Handle login error
+//       if (error.response && error.response.data && error) {
+//         setError(error.response.data.message);
+//       } else {
+//         setError("An error occurred while logging in. Please try again.");
+//       }
+//     }
+//   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input
-          type="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      {error && <div className="error-message">{error}</div>}
-      <button type="submit">Login</button>
-    </form>
-  );
-};
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label>Username:</label>
+//         <input
+//           type="username"
+//           value={username}
+//           onChange={(e) => setUsername(e.target.value)}
+//           required
+//         />
+//       </div>
+//       <div>
+//         <label>Password:</label>
+//         <input
+//           type="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           required
+//         />
+//       </div>
+//       {error && <div className="error-message">{error}</div>}
+//       <button type="submit">Login</button>
+//     </form>
+//   );
+// };
 
-export default LoginForm;
+// export default LoginForm;
--- src/modals/RegisterModal.jsx
+++ src/modals/RegisterModal.jsx
@@ -1,10 +1,79 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import axios from "../axios";
 import { ModalContext } from "../pages/_TemplatePage";
 import LoginModal from "./LoginModal";
-import RegisterForm from "../components/RegisterForm";
+// import { useNavigate } from "react-router-dom";
 
 const RegisterModal = () => {
   const { closeModal, openModal } = useContext(ModalContext);
+  // const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [location, setLocation] = useState("");
+  const [securityQuestionOne, setSecurityQuestionOne] = useState("");
+  const [securityQuestionTwo, setSecurityQuestionTwo] = useState("");
+  const [securityQuestionThree, setSecurityQuestionThree] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    event.stopPropagation(); // Prevent event bubbling
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `/user/register`,
+        {
+          username,
+          email,
+          password,
+          location,
+          securityQuestionOne,
+          securityQuestionTwo,
+          securityQuestionThree,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Register successful:", response.data);
+
+      // Clear form data so it is clear to user that registration is successfully submitted
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setLocation("");
+      setSecurityQuestionOne("");
+      setSecurityQuestionTwo("");
+      setSecurityQuestionThree("");
+      setError(null);
+
+      closeModal();
+      openModal(<LoginModal />);
+    } catch (error) {
+      console.error("Error signing up:", error);
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message); // Display error message from backend
+      } else {
+        setError("An error occurred while signing up. Please try again."); // Display generic error message
+      }
+    }
+  };
 
   const handleLoginClick = (event) => {
     event.preventDefault();
@@ -20,7 +89,80 @@ const RegisterModal = () => {
           Login!
         </span>
       </p>
-      <RegisterForm />
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Location:</label>
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Security Question 1:</label>
+          <input
+            type="text"
+            value={securityQuestionOne}
+            onChange={(e) => setSecurityQuestionOne(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Security Question 2:</label>
+          <input
+            type="text"
+            value={securityQuestionTwo}
+            onChange={(e) => setSecurityQuestionTwo(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Security Question 3:</label>
+          <input
+            type="text"
+            value={securityQuestionThree}
+            onChange={(e) => setSecurityQuestionThree(e.target.value)}
+          />
+        </div>
+        {error && <div className="error-message">{error}</div>}{" "}
+        {/* Display error message if there is an error */}
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 };
