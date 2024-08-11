22ee159d3489a4e6e2eff92d237e17460fbb17da
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-08-11 18:48:02 +1000

    fix: updated render deploy url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
-export const API_BASE_URL = "https://gameplan-backend.onrender.com";
+export const API_BASE_URL = "https://gameplan-backend-7j9b.onrender.com";
 
 export const getToken = () => localStorage.getItem("token");
